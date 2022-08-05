19208aa24cf8337f63e02d5c53fc17dd03c4678f
Asileka <asilkatya@gmail.com>
2022-08-05 15:44:21 +0100

    kats app

--- App.js
+++ App.js
@@ -1,11 +1,53 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
+import logo from "./assets/logo.png";
+import * as ImagePicker from "expo-image-picker";
 
 export default function App() {
+  const [selectedImage, setSelectedImage] = React.useState(null);
+  let openImagePickerAsync = async () => {
+    let permissionResult =
+      await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if (permissionResult.granted === false) {
+      alert("Permission to access camera roll is required!");
+      return;
+    }
+
+    let pickerResult = await ImagePicker.launchImageLibraryAsync();
+    console.log(pickerResult);
+    if (pickerResult.cancelled === true) {
+      return;
+    }
+
+    setSelectedImage({ localUri: pickerResult.uri });
+  };
+
+  if (selectedImage !== null) {
+    return (
+      <View style={styles.container}>
+        <Image source={logo} style={styles.logo} />
+        <Text style={styles.text}>Katia's first mobile app</Text>
+        <StatusBar style="auto" />
+        <TouchableOpacity onPress={openImagePickerAsync} style={styles.button}>
+          <Text style={styles.buttonText}>Pick a photo</Text>
+        </TouchableOpacity>
+        <Image
+          source={{ uri: selectedImage.localUri }}
+          style={styles.thumbnail}
+        />
+      </View>
+    );
+  }
   return (
     <View style={styles.container}>
-      <Text>Katia's first mobile app</Text>
+      <Image source={logo} style={styles.logo} />
+      <Text style={styles.text}>Katia's first mobile app</Text>
       <StatusBar style="auto" />
+      <TouchableOpacity onPress={openImagePickerAsync} style={styles.button}>
+        <Text style={styles.buttonText}>Pick a photo</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -13,8 +55,33 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#fcd7df",
     alignItems: "center",
     justifyContent: "center",
+  },
+  logo: {
+    width: 305,
+    height: 250,
+    marginBottom: 10,
+  },
+  text: {
+    color: "#888",
+    fontSize: 18,
+    marginHorizontal: 15,
+  },
+  button: {
+    backgroundColor: "magenta",
+    padding: 20,
+    borderRadius: 5,
+    marginTop: 10,
+  },
+  buttonText: {
+    fontSize: 20,
+    color: "#fff",
+  },
+  thumbnail: {
+    width: 300,
+    height: 300,
+    resizeMode: "contain",
   },
 });
